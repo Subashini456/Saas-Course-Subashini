e815087af8eebe61ed22961a5ca36bcefebf7d38
Subashini <subashiniravi492@gmail.com>
2021-02-26 14:05:08 +0530

    my first ruby program

---
+++ todo.rb
@@ -0,0 +1,24 @@
+#puts - ask something from user
+
+puts "hello,what is ur todo list tdy"
+
+#gets - gets the string input from user
+
+gets
+
+#variable (next_todo) - gets input n store in a varaiable
+
+next_todo = gets
+
+puts "here is ur next todo"
+puts next_todo
+
+#carriage return - adds extra line
+
+puts "\n\n"
+
+#string interpolation - write a string & puts ruby expression
+
+puts "hello everyone #{"hi"}"
+puts "hello everyone #{1 + 1}"
+puts "hello everyone #{next_todo}"
